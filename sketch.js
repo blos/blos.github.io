d5d4538ab4ee5d5d325b64231dec7bfbbb469a66
Matthias Lang <matthias.lang@student.hs-rm.de>
2020-04-09 10:58:19 +0200

    small updates done on some things: -optimization: --using 'const' on some variables --reimplemented all counters -now measuring the time it takes to render one frame in ms -dead object don't interact with other objects -dead object don't move anymore

--- sketch.js
+++ sketch.js
@@ -1,29 +1,31 @@
 /* PLAY WITH THESE VALUES IF YOU WANT */
 
-//determines the size of the canvas 
+//determines the SIZE of the canvas 
 //       x ,  y
-size = [500, 500];
+const SIZE = [500, 500];
 
-NUM = 2000;  //number of objects which will generated, be carefull here
-LEN = 4;     //size of each object
-DAMPING = .8;//moving velocity
+const NUM = 500;      //number of objects which will be generated, be carefull here
+const LEN = 4;        //size of each object
+const DAMPING = 1.5;  //moving speed
 
-HEAL_RANGE = 15; //range of heal-time
-HEAL_OFFSET = 5; //start of the range of heal time
+const HEAL_RANGE = 15;   //range of HEAL-time
+const HEAL_OFFSET = 1.5; //start of the range of HEAL time
 
-INIT_INFECTED = 1;  //amount of initial inected objects
-REINFECTION_POT = .05; //probability of getting reinfected when healthy
+const INIT_INFECTED = 1;       //amount of initial inected objects
+const REINFECTION_POT = .05;   //probability of getting reINFECTED when HEALthy
 /* UNTIL HERE */
 
 
 /********* DO NOT TOUCH THESE VARIABLES *********/
 INIT_INFECT_TIME = false;
 
-FPS = 0;
+FT = 0;
 SECONDS = 0;
 
-SPAWN_OFFSET = 20;
+const SPAWN_OFFSET = 20;
 
+const TIME_START = new Date();
+TIME_CURR = 0;
 
 XS = [];
 SAVE_UNINF = [];
@@ -31,18 +33,22 @@ SAVE_INF = [];
 SAVE_HEALTH = [];
 SAVE_DEAD = [];
 
+POSX = [];
+POSY = [];
 
-posx = [];
-posy = [];
+VELX = [];
+VELY = [];
 
-velx = [];
-vely = [];
+COLORS = [];
+INFECTED = [];
+HEAL = [];
 
-colors = [];
-infected = [];
-heal = [];
+FC = 0;
 
-fc = 0;
+COUNT_UNINF = 0;
+COUNT_INF = 0;
+COUNT_HEALTHY = 0;
+COUNT_DEAD = 0;
 /********* UNTIL HERE *********/
 
 
@@ -51,7 +57,7 @@ fc = 0;
 function setup() {
 
   //creates the canvas
-  createCanvas(size[0], size[1]);
+  createCanvas(SIZE[0], SIZE[1]);
 
   //iterates over all objects just to initalize everything
   for (let i = 0; i < NUM; i++) {
@@ -59,22 +65,28 @@ function setup() {
     let dirx = Math.random() >= .5 ? 1 : -1;
     let diry = Math.random() >= .5 ? 1 : -1;
     //starting position of the objects
-    posx[i] = SPAWN_OFFSET + Math.floor((size[0] - 2 * SPAWN_OFFSET) * random());
-    posy[i] = SPAWN_OFFSET + Math.floor((size[1] - 2 * SPAWN_OFFSET) * random());
+    POSX[i] = SPAWN_OFFSET + Math.floor((SIZE[0] - 2 * SPAWN_OFFSET) * random());
+    POSY[i] = SPAWN_OFFSET + Math.floor((SIZE[1] - 2 * SPAWN_OFFSET) * random());
     //random velocity for the objects
-    velx[i] = dirx * random() * DAMPING;
-    vely[i] = diry * random() * DAMPING;
-    //determines which time every object need to heal
-    heal[i] = HEAL_OFFSET + Math.round(random(1, HEAL_RANGE));
-    //initializing every object as uninfected
-    colors[i] = 0;
-    infected[i] = 0;
+    VELX[i] = dirx * random() * DAMPING;
+    VELY[i] = diry * random() * DAMPING;
+    //determines which time every object need to HEAL
+    HEAL[i] = HEAL_OFFSET + Math.round(random(1, HEAL_RANGE));
+    //initializing every object as unINFECTED
+    COLORS[i] = 0;
+    INFECTED[i] = 0;
   }
 
   //infects initial objects with the number given by INIT_INFECTED
   for (let i = 0; i < INIT_INFECTED; i++) {
-    colors[i] = 1;
+    COLORS[i] = 1;
   }
+
+  //sets up neccessary numbers for correct counting
+  COUNT_INF = INIT_INFECTED;
+  COUNT_UNINF = NUM - COUNT_INF;
+
+  TIME_CURR = TIME_START;
 
 }
 
@@ -82,129 +94,139 @@ function setup() {
 function draw() {
 
   //some vars which will be updated every frame
-  fc = frameCount;
-  FPS = fc % 20 == 0 ? frameRate().toFixed(2) : FPS; //updates every 20 frames the FPS-counter
-  SECONDS = (fc / FPS).toFixed(2);
+  FC = frameCount;
+  FT = new Date() - TIME_CURR; //updates every 20 frames the FT-counter
+  TIME_CURR = new Date();
+  SECONDS = (new Date() - TIME_START) / 1000;
 
   //flips the flag if there were more than 200 frames drawn
-  if (fc > 200) INIT_INFECT_TIME = true;
+  if (FC > 100) INIT_INFECT_TIME = true;
 
-  //resets the background and drawing colors
+  //resets the background and drawing COLORS
   background(0);
-  fill(255);
 
   //iterates over every object
   for (let i = 0; i < NUM; i++) {
 
     //updating the position so the objects are moving
-    posx[i] = posx[i] + velx[i];
-    posy[i] = posy[i] + vely[i];
+    POSX[i] = POSX[i] + VELX[i];
+    POSY[i] = POSY[i] + VELY[i];
 
     //coloring the objects according to the states of the objects
-    if (colors[i] == 0) { //init state
-      fill(255);
-    } else if (colors[i] == 1) {  //infected or init_infected state
-      fill(255, 0, 0);
-    } else if (colors[i] == 2) {  //healthy state
-      fill(0, 0, 255);
-    } else if (colors[i] == 3) {  //dead state
-      fill(255, 0, 255);
+    if (COLORS[i] == 0) {         //init state
+      fill(255);//white
+    } else if (COLORS[i] == 1) {  //INFECTED or init_INFECTED state
+      fill(255, 0, 0);//red
+    } else if (COLORS[i] == 2) {  //HEALthy state
+      fill(0, 0, 255);//blue
+    } else if (COLORS[i] == 3) {  //dead state
+      fill(255, 0, 255);//pink
     }
 
-    //draws the object on posx and posy with length of each side of LEN
-    square(posx[i], posy[i], LEN);
+    //draws the object on POSX and POSY with length of each side of LEN
+    square(POSX[i], POSY[i], LEN);
 
     //checks if the objects are inside of canvas
     borderCheck(i);
 
     //iterates over all object beginning with the i+1-index
     for (let j = i + 1; j < NUM; j++) {
+
       //if they touch
       if (touch(i, j)) {
+
+        //handles the interaction with dead objects and not-dead objects
+        if (COLORS[i] == 3) {
+          //invertVel(j);
+          continue;
+        } else if (COLORS[j] == 3) {
+          //invertVel(i);
+          continue;
+        }
+
         //then swap their velocities
         swapVel(i, j);
 
         //if they are objects in the same state no actions needed because
         //nothing should happen
         //its a small optimization which should not have that big impact
-        if (colors[i] == 1 && colors[j] == 1) continue;
-        if (colors[i] == 2 && colors[j] == 2) continue;
-        if (colors[i] == 3 && colors[j] == 3) continue;
+        if (COLORS[i] == 1 && COLORS[j] == 1) continue;
+        if (COLORS[i] == 2 && COLORS[j] == 2) continue;
+        if (COLORS[i] == 3 && COLORS[j] == 3) continue;
 
-        //this makes reinfaction of healthy object possible
+        //this makes reinfaction of HEALthy object possible
         //by generating a random number and checking if that number
         //is smaller than REINFECTION_POTential
-        if (colors[i] == 2 && colors[j] == 1) {
+        if (COLORS[i] == 2 && COLORS[j] == 1) {
           let tmp = random();
           if (tmp < REINFECTION_POT) {
-            colors[i] = 1;
-            infected[i] = SECONDS;
+            COLORS[i] = 1;
+            INFECTED[i] = SECONDS;
+            COUNT_HEALTHY--;  //decreace healthy count
+            COUNT_INF++;      //increace infected count
           }
         }
         //same as directly above but reversed rolles
         //should not be necessary
-        if (colors[j] == 2 && colors[i] == 1) {
+        if (COLORS[j] == 2 && COLORS[i] == 1) {
           let tmp = random();
           if (tmp < REINFECTION_POT) {
-            colors[j] = 1;
-            infected[j] = SECONDS;
+            COLORS[j] = 1;
+            INFECTED[j] = SECONDS;
+            COUNT_HEALTHY--;  //decreace healthy count
+            COUNT_INF++;      //increace infected count
           }
         }
 
-        //infects the uninfected object when an uninfected and
-        //a infected object are hitting each other
-        if (colors[i] == 1 && colors[j] == 0) {
-          colors[j] = 1;
-          infected[j] = SECONDS;
+        //infects the unINFECTED object when an unINFECTED and
+        //a INFECTED object are hitting each other
+        if (COLORS[i] == 1 && COLORS[j] == 0) {
+          COLORS[j] = 1;
+          INFECTED[j] = SECONDS;
+          COUNT_UNINF--;
+          COUNT_INF++;
         }
         //same as directly above but reversed rolles
         //should not be necessary
-        if (colors[j] == 1 && colors[i] == 0) {
-          colors[i] = 1;
-          infected[i] = SECONDS;
+        if (COLORS[j] == 1 && COLORS[i] == 0) {
+          COLORS[i] = 1;
+          INFECTED[i] = SECONDS;
+          COUNT_UNINF--;
+          COUNT_INF++;
         }
 
       }
     }
 
-    //heals up infected objects only if the time which needs
-    //to heal elapsed
-    //and a the healing is possible due to the 'INIT_INFECT_TIME'-flag
-    if (INIT_INFECT_TIME && colors[i] == 1 && (SECONDS - infected[i] >= heal[i])) {
-      colors[i] = 2;
+    //HEALs up INFECTED objects only if the time which needs
+    //to HEAL elapsed
+    //and if HEALing is possible due to the 'INIT_INFECT_TIME'-flag
+    if (INIT_INFECT_TIME && COLORS[i] == 1 && (SECONDS - INFECTED[i] >= HEAL[i])) {
+      COLORS[i] = 2;
+      COUNT_INF--;
+      COUNT_HEALTHY++;
     }
 
   }
 
-  //the idea is that every infected object can die
+  //the idea is that every INFECTED object can die
   //so a random number from all will be selected and if it is
-  //infected, this object dies
+  //INFECTED, this object dies
   let tmp = Math.round(random(1, NUM));
-  if (colors[tmp] == 1) {
-    colors[tmp] = 3;
+  if (COLORS[tmp] == 1) {
+    COLORS[tmp] = 3;
+    VELX[tmp] = 0;
+    VELY[tmp] = 0;
+    COUNT_INF--;
+    COUNT_DEAD++;
   }
 
   //status output for basic render observation
   fill(0, 255, 0);
   stroke(0);
-  text("FPS: " + FPS, 10, height - 10);
-  text("Framecount: " + fc, 10, height - 25);
+  text("Frametime: " + FT, 10, height - 10);
+  text("Framecount: " + FC, 10, height - 25);
   text("Seconds: " + SECONDS, 10, height - 40);
-
-  //resetting the values every frame
-  COUNT_UNINF = 0;
-  COUNT_INF = 0;
-  COUNT_DEAD = 0;
-  COUNT_HEALTHY = 0;
-
-  //recounts the states of every object every frame
-  //very slow but i'm lazy
-  for (let i = 0; i < NUM; i++) {
-    if (colors[i] == 0) COUNT_UNINF++;
-    if (colors[i] == 1) COUNT_INF++;
-    if (colors[i] == 2) COUNT_HEALTHY++;
-    if (colors[i] == 3) COUNT_DEAD++;
-  }
 
   //renders the information of the conditions
   fill(255);
@@ -215,22 +237,23 @@ function draw() {
   text("dead: " + COUNT_DEAD, width - 100, height - 55);
 
   //pushes the values into an array
-  XS.push(fc);
+  XS.push(SECONDS);
   SAVE_UNINF.push(COUNT_UNINF);
   SAVE_INF.push(COUNT_INF);
   SAVE_HEALTH.push(COUNT_HEALTHY);
   SAVE_DEAD.push(COUNT_DEAD);
 
 
-  //ends the simulation if there are more than 15000 frames drawn or
-  //the counter for the infected objects are equals to 0
-  if (fc > 15000 || COUNT_INF == 0) {
+  //ends the simulation if there are more than 15.000 frames drawn or
+  //the counter for the INFECTED objects are equals to 0
+  if (FC >= 15000 || COUNT_INF == 0) {
     //stops the looping
     noLoop();
     //formats the json file
-    let saveOut = { X: XS, UNINF: SAVE_UNINF, INF: SAVE_INF, HEALTHY: SAVE_HEALTH, DEAD: SAVE_DEAD };
+    let saveOut = { TIME: XS, UNINF: SAVE_UNINF, INF: SAVE_INF, HEALTHY: SAVE_HEALTH, DEAD: SAVE_DEAD };
     //saving into a json-file
-    saveJSON(saveOut, 'test.json', true);
+    let saveName = new Date();
+    saveJSON(saveOut, 'simulation_export_' + saveName.getFullYear() + '_' + (saveName.getMonth() + 1) + '_' + saveName.getDate() + '_' + '.json', true);
   }
 
 }
@@ -240,12 +263,21 @@ function draw() {
  * @param {*} i index of the object which should be checked
  */
 function borderCheck(i) {
-  if (posx[i] + LEN > size[0] || posx[i] < 0) {
-    velx[i] = -1 * velx[i];
+  if (POSX[i] + LEN > SIZE[0] || POSX[i] < 0) {
+    VELX[i] = -1 * VELX[i];
     return;
-  } else if (posy[i] + LEN > size[1] || posy[i] < 0) {
-    vely[i] = -1 * vely[i];
+  } else if (POSY[i] + LEN > SIZE[1] || POSY[i] < 0) {
+    VELY[i] = -1 * VELY[i];
   }
+}
+
+/**
+ * inverts the direction of the velocity of a object
+ * @param {*} i indec of the object
+ */
+function invertVel(i) {
+  VELX[i] = -1 * VELX[i];
+  VELY[i] = -1 * VELY[i];
 }
 
 /**
@@ -255,15 +287,17 @@ function borderCheck(i) {
  * @param {*} j index of the other object
  */
 function swapVel(i, j) {
-  tmp = velx[i];
-  velx[i] = velx[j];
-  velx[j] = tmp;
+  tmp = VELX[i];
+  VELX[i] = VELX[j];
+  VELX[j] = tmp;
 
-  tmp = vely[i];
-  vely[i] = vely[j];
-  vely[j] = tmp;
+  tmp = VELY[i];
+  VELY[i] = VELY[j];
+  VELY[j] = tmp;
 }
 
+
+/******* handles the collision detection of the objects *******/
 function touch(i, j) {
   return !(isLeftOf(i, j)
     || isRightOf(i, j)
@@ -272,7 +306,7 @@ function touch(i, j) {
 }
 
 function isAbove(i, j) {
-  return (posy[i] + LEN) < posy[j];
+  return (POSY[i] + LEN) < POSY[j];
 }
 
 function isUnderneath(i, j) {
@@ -280,7 +314,7 @@ function isUnderneath(i, j) {
 }
 
 function isLeftOf(i, j) {
-  return (posx[i] + LEN) < posx[j];
+  return (POSX[i] + LEN) < POSX[j];
 }
 
 function isRightOf(i, j) {
